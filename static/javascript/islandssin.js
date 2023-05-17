9d36197f9a7d3ec2109f4b2ebfa7980e0ce7f489
nene <neneno_5@yahoo.co.jp>
2023-05-17 18:36:04 +0900

    change islandssin.js

--- static/javascript/islandssin.js
+++ static/javascript/islandssin.js
@@ -1,13 +1,63 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('nav ul li');
-    
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
-            navLinks.forEach(function(item) {
-                item.classList.remove('current');
-            });
-            
-            this.classList.add('current');
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    var currentIndex = 0;
+    var maxIndex = 5; // ページの総数 - 1
+
+    function showPage(index) {
+        var pages = document.querySelectorAll(".page");
+        for (var i = 0; i < pages.length; i++) {
+            pages[i].style.display = "none";
+        }
+        pages[index].style.display = "block";
+    }
+
+    function setActiveNavItem(index) {
+        var navItems = document.querySelectorAll("nav ul li");
+        for (var i = 0; i < navItems.length; i++) {
+            navItems[i].classList.remove("active");
+        }
+        navItems[index].classList.add("active");
+    }
+
+    function navigateToNextPage() {
+        if (currentIndex < maxIndex) {
+            currentIndex++;
+            showPage(currentIndex);
+            setActiveNavItem(currentIndex);
+        }
+    }
+
+    function navigateToPreviousPage() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showPage(currentIndex);
+            setActiveNavItem(currentIndex);
+        }
+    }
+
+    document.addEventListener("touchstart", function(event) {
+        startX = event.changedTouches[0].screenX;
     });
+
+    document.addEventListener("touchend", function(event) {
+        var endX = event.changedTouches[0].screenX;
+        var diffX = endX - startX;
+        if (diffX > 50) {
+            navigateToPreviousPage();
+        } else if (diffX < -50) {
+            navigateToNextPage();
+        }
+    });
+
+    var navItems = document.querySelectorAll("nav ul li");
+    for (var i = 0; i < navItems.length; i++) {
+        navItems[i].addEventListener("click", function() {
+            var index = Array.from(navItems).indexOf(this);
+            currentIndex = index;
+            showPage(currentIndex);
+            setActiveNavItem(currentIndex);
+        });
+    }
+
+    showPage(currentIndex);
+    setActiveNavItem(currentIndex);
 });
