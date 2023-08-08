5eeb7e41d7ae37957b1c850d1bd494e73b9442ef
nene <neneno_5@yahoo.co.jp>
2023-08-08 16:24:37 +0900

    correction

--- static/javascript/shiori.js
+++ static/javascript/shiori.js
@@ -45,5 +45,6 @@ document.addEventListener("DOMContentLoaded", function() {
             showPage(currentPage + 1); // 次のページを表示
         } else if (swipeDistance < -minSwipeDistance && currentPage == 3) {
             showPage(currentPage - 2); // 次のページを表示 
+        }
     });
 });
