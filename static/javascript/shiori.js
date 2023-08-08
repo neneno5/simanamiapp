c237e885b44598dcc14449bd9df7a83ac1d7e248
nene <neneno_5@yahoo.co.jp>
2023-08-08 16:48:04 +0900

    change  shiori.js

--- static/javascript/shiori.js
+++ static/javascript/shiori.js
@@ -43,6 +43,8 @@ document.addEventListener("DOMContentLoaded", function() {
             showPage(currentPage - 1); // 前のページを表示
         } else if (swipeDistance < -minSwipeDistance && currentPage < 3) {
             showPage(currentPage + 1); // 次のページを表示
+        } else if (swipeDistance < -minSwipeDistance && currentPage == 3) {
+            showPage(currentPage - 2);
         }
     });
 });
